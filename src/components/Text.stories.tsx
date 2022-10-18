122d5e138f3f8c6dd3c4caf45e61d6ab9ed03861
Thalyson Luiz <thalysonluiz@gmail.com>
2022-10-18 11:21:45 -0300

    Estilização Comp Text e Storybook

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -1,8 +1,22 @@
-import { Text } from './Text'
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from './Text'
 
 export default {
   title: 'Components/Text',
   component: Text,
-}
+  args: {
+    children: 'Lorem ipsun'
+  }
+} as Meta<TextProps>
 
-export const Default = {}
+export const Default: StoryObj<TextProps> = {}
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: 'sm'
+  }
+}
+export const Large: StoryObj<TextProps> = {
+  args: {
+    size: 'lg'
+  }
+}
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,5 +1,20 @@
-export function Text() {
+import { clsx } from 'clsx'
+
+export interface TextProps {
+  size?: 'sm' | 'md' | 'lg';
+  children: string;
+}
+
+export function Text({ size = 'md', children }: TextProps) {
   return (
-    <span>Text</span>
+    <span
+      className={clsx("text-gray-100 font-sans", {
+        'text-xs': size === 'sm',
+        'text-sm': size === 'md',
+        'text-md': size === 'lg',
+      })}
+    >
+      {children}
+    </span>
   )
 }
