7fe65eaa2d29fecb18b4acbdca0d83fab951d621
Thalyson Luiz <thalysonluiz@gmail.com>
2022-10-18 10:54:33 -0300

    Component Text com Storybook

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,4 +1,4 @@
-import { addons } from '@storybook/addon'
+import { addons } from '@storybook/addons'
 import { themes } from '@storybook/theming'
 
 addons.setConfig({
---
+++ src/components/Text.tsx
@@ -0,0 +1,5 @@
+export function Text() {
+  return (
+    <span>Text</span>
+  )
+}
